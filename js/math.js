737b44be2cd5c4b63b8a8b5e7a9802df7383b410
wasifaislam88 <wasifaislam88@gmail.com>
2025-01-05 20:24:01 +0600

    added a commit

--- js/math.js
+++ js/math.js
@@ -1,10 +1,1 @@
 
-// donate card Start.....
-
-document.getElementById("donate-btn-one").addEventListener("click",function(){
-    const donateInputOne = getInputValueById("donate-input-one");
-    const totalDonateOne = getTotalAmountByid("total-donate-one");
-});
-
-const balance = getTotalAmountByid(balance);
-
