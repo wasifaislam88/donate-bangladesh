103600d0bec02549d3ee86c9147290fe2c5a826f
wasifaislam88 <wasifaislam88@gmail.com>
2025-01-07 17:15:46 +0600

    added a commit

--- js/math.js
+++ js/math.js
@@ -1,1 +1,174 @@
 
+// console.log("test..........")
+
+// donate card start..
+
+
+document.getElementById("donate-one-btn").addEventListener("click",function(){
+    const donateInputOne = document.getElementById("donate-input-one").value;
+    const totalDonateOne = document.getElementById("donate-one").innerText;
+    const balance = document.getElementById("balance").innerText;
+
+
+    if(Number(balance)<=5500){
+        alert("your account does not have sufficient balance");
+        return;
+    }
+
+    else if( donateInputOne < 1 ||isNaN(Number(donateInputOne)) || Number(balance) < Number(donateInputOne)) {
+        alert("invalid donation amount");
+        
+        document.getElementById("donate-input-one").value= "";
+        return;
+    }
+
+
+    const newTotalDonateOne = Number(totalDonateOne) + Number(donateInputOne);
+    console.log(newTotalDonateOne)
+
+
+    document.getElementById("donate-one").innerText = newTotalDonateOne;
+
+
+    document.getElementById("donate-input-one").value="";
+
+
+    const newBalance = Number(balance) - donateInputOne;
+    // console.log(newBalance)
+    // console.log(typeof newBalance)
+
+    document.getElementById("balance").innerText = newBalance;
+});
+
+// --------------------------->card 2<-------------------->
+
+
+document.getElementById("donate-two-btn").addEventListener("click",function(){
+
+
+    const donateInputTwo = document.getElementById("donate-input-two").value;
+    const totalDonateTwo = document.getElementById("donate-two").innerText;
+    const balance = document.getElementById("balance").innerText;
+    console.log("test.....")
+    console.log(donateInputTwo)
+    console.log(totalDonateTwo)
+    console.log( balance)
+
+
+    if(Number(balance)<=5500){
+        alert("your account does not have sufficient balance");
+        return;
+    }
+
+    else if(donateInputTwo  < 1 ||isNaN(Number(donateInputTwo)) || Number(balance) < Number(donateInputTwo)) {
+        alert("invalid donation amount");
+        
+        document.getElementById("donate-input-two").value= "";
+        return;
+    }
+
+    const newTotalDonateTwo = Number(totalDonateTwo) + Number(donateInputTwo);
+    console.log(newTotalDonateTwo)
+
+
+    document.getElementById("donate-two").innerText = newTotalDonateTwo;
+
+
+    document.getElementById("donate-input-two").value="";
+
+
+    const newBalance = Number(balance) - donateInputTwo;
+
+
+    document.getElementById("balance").innerText = newBalance;
+});
+
+
+// ------------>card 3---<---------------->
+
+
+
+document.getElementById("donate-three-btn").addEventListener("click",function(){
+
+
+    const donateInputThree = document.getElementById("donate-input-three").value;
+    const totalDonateThree = document.getElementById("donate-three").innerText;
+    const balance = document.getElementById("balance").innerText;
+    // console.log("test.....")
+    // console.log(donateInputThree)
+    // console.log(newTotalDonateThree)
+    // console.log( balance)
+
+
+    if(Number(balance)<=5500){
+        alert("your account does not have sufficient balance");
+        return;
+    }
+
+   else if(donateInputThree  < 1 ||isNaN(Number(donateInputThree)) || Number(balance) < Number(donateInputThree)) {
+        alert("invalid donation amount");
+        
+        document.getElementById("donate-input-three").value= "";
+        return;
+    }
+
+
+    const newTotalDonateThree = Number(totalDonateThree) + Number(donateInputThree);
+    console.log(newTotalDonateThree)
+
+
+    document.getElementById("donate-three").innerText = newTotalDonateThree;
+
+
+    document.getElementById("donate-input-three").value="";
+
+
+    const newBalance = Number(balance) - donateInputThree;
+
+
+    document.getElementById("balance").innerText = newBalance;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById("donate-one-btn").addEventListener("click", function() {
+//     const donatInputOne = document.getElementById("donate-input-one");
+//     const totalDonateOne = document.getElementById("donate-one");
+//     const balance = document.getElementById("balance");
+//     const d = document.getElementById("donate-input-one");
+
+//     console.log('nahian');
+// });
